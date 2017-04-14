bc13c72324ccfd0e7226d5ee2a99aeba615501e7
Chris Colicino <ccolicino@gmail.com>
2017-04-13 21:31:26 -0400

    Added queue

--- DataStructures/Queue.cpp
+++ DataStructures/Queue.cpp
@@ -1,12 +1,38 @@
 #include "Queue.h"
+#include <exception>
 
-
-
-Queue::Queue()
+template<class T> Queue<T>::Queue()
 {
+	Head = NULL;
+	Tail = NULL;
 }
 
 
-Queue::~Queue()
+template<class T> Queue<T>::~Queue()
 {
+}
+
+template<class T> void Queue<T>::Push(T &element)
+{
+	Node<T> *node = new Node<T>();
+	node->Element = element;
+	if (Tail == NULL) {
+		Head = node;
+		Tail = node;
+	}
+	else {
+		Tail->Next = node;
+		Tail = node;
+	}
+}
+
+template<class T> T Queue<T>::Pop()
+{
+	if (Head == NULL)
+		throw new std::exception("No element at Head");
+	Node<T> *node = Head;
+	T element = node->Element;
+	Head = Head->Next;
+	delete node;
+	return element;
 }
--- DataStructures/Queue.h
+++ DataStructures/Queue.h
@@ -1,8 +1,22 @@
 #pragma once
-class Queue
+
+template<class T> class Queue
 {
+	template<class T> class Node 
+	{
+	public:
+		Node() {Next = NULL};
+		T Element;
+		Node<T> *Next;
+	};
+
+private:
+	Node<T> *Head;
+	Node<T> *Tail;
 public:
 	Queue();
 	~Queue();
+	void Push(T &element);
+	T Pop();
 };
 
